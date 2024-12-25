643e7e26afc4130bfa1bd3ed99f7aa7d27f9dea1
alsqja <alsqha2626@gmail.com>
2024-12-25 11:54:20 +0900

    feat: 비밀번호 확인 구현
    - logout 추가 (기존 토큰에 대한 관리 필요)
    - 비밀번호 확인 여부 쿠키로 전송 (만료시간 5분 설정)

---
+++ src/main/java/com/example/panttegi/user/dto/PasswordReqDto.java
@@ -0,0 +1,11 @@
+package com.example.panttegi.user.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class PasswordReqDto {
+
+    private final String password;
+}
