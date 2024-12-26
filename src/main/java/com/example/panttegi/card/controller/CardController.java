ae3b03b57e7253434e3800f87e1a3ffc2e38be19
kimdanbin <rlaeksqls@naver.com>
2024-12-26 10:01:12 +0900

    feat: 카드 생성

--- src/main/java/com/example/panttegi/card/controller/CardController.java
+++ src/main/java/com/example/panttegi/card/controller/CardController.java
@@ -4,13 +4,12 @@ import com.example.panttegi.card.dto.PostCardRequestDto;
 import com.example.panttegi.card.dto.PostCardResponseDto;
 import com.example.panttegi.card.service.CardService;
 import com.example.panttegi.common.CommonResDto;
+import com.example.panttegi.user.entity.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -21,18 +20,25 @@ public class CardController {
     // 카드 생성
     @PostMapping
     public ResponseEntity<CommonResDto<PostCardResponseDto>> postCard(
-            @RequestBody PostCardRequestDto postCardRequestDto
+            @Valid @RequestBody PostCardRequestDto postCardRequestDto,
+            @RequestHeader("Authorization") String token
     ) {
+
+        // 나중에 토큰으로 받음
+        Long userId = 1L;
 
         PostCardResponseDto card = cardService.postCard(
                 postCardRequestDto.getTitle(),
                 postCardRequestDto.getDescription(),
                 postCardRequestDto.getPosition(),
                 postCardRequestDto.getEndAt(),
+                userId,
                 postCardRequestDto.getManagerId(),
                 postCardRequestDto.getBoardListId()
         );
 
         return new ResponseEntity<>(new CommonResDto<>("카드 생성 완료", card), HttpStatus.CREATED);
     }
+
+
 }
