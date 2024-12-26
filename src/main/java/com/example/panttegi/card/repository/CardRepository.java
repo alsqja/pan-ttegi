e3f448af44cac314b874eb4cfc32bc70f6b510d2
kimdanbin <rlaeksqls@naver.com>
2024-12-26 10:21:51 +0900

    refactor: findByIdOrElseThrow 추가

--- src/main/java/com/example/panttegi/card/repository/CardRepository.java
+++ src/main/java/com/example/panttegi/card/repository/CardRepository.java
@@ -1,9 +1,15 @@
 package com.example.panttegi.card.repository;
 
 import com.example.panttegi.card.entity.Card;
+import com.example.panttegi.error.errorcode.ErrorCode;
+import com.example.panttegi.error.exception.CustomException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    default Card findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+    }
 }
