ea937afada282f595d037aab62dd8a7eaed2856e
kimdanbin <rlaeksqls@naver.com>
2024-12-27 08:07:13 +0900

    refactor: 댓글 dto 수정

--- src/main/java/com/example/panttegi/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/panttegi/comment/dto/CommentRequestDto.java
@@ -1,14 +1,20 @@
 package com.example.panttegi.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CommentRequestDto {
 
     @NotBlank(message = "BAD_INPUT")
     private final String content;
+
+    @JsonCreator
+    public CommentRequestDto(String content) {
+        this.content = content;
+    }
 
 }
