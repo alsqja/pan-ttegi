f8e06d381b02f8ae6ef5e87cae76709a39dbab56
kimdanbin <rlaeksqls@naver.com>
2024-12-27 19:40:25 +0900

    factor: path 수정

--- src/main/java/com/example/panttegi/card/controller/CardController.java
+++ src/main/java/com/example/panttegi/card/controller/CardController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cards")
+@RequestMapping("/api/workspaces/{workspaceId}/cards")
 @RequiredArgsConstructor
 public class CardController {
     private final CardService cardService;
